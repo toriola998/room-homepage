454cdb02b080a568273d22d188b53b215b5c3bcd
Toriola Faidat <faaedah@gmail.com>
2021-11-28 18:39:31 +0100

    cleaned up html file and set up array in js

--- main.js
+++ main.js
@@ -19,3 +19,18 @@ navLinks.forEach(link => {
     links.classList.remove('show-links');
   }); 
 });
+
+const info = [
+   {
+    img: "./images/mobile-image-hero-2.jpg",
+    heading: 'We are available all across the globe',
+    productInfo: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today." ,
+},
+
+{
+    img: "./images/mobile-image-hero-2.jpg",
+    heading: 'Manufactured with the best materials',
+    productInfo: " Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
+
+}
+]

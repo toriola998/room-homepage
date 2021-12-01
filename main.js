27833c2a13c72332a248ddaf2cd46ebbcf28e5b7
Toriola Faidat <faaedah@gmail.com>
2021-12-01 21:57:34 +0100

    linked js file to html

--- main.js
+++ main.js
@@ -23,21 +23,21 @@ navLinks.forEach(link => {
 const info = [
     {
         id: 1,
-        img: "./images/mobile-image-hero-1.jpg",
+        img: "./images/desktop-image-hero-1.jpg",
         heading: 'Discover innovative ways to decorate',
         product: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love." ,
     },
 
    {
         id: 2,
-        img: "./images/mobile-image-hero-2.jpg",
+        img: "./images/desktop-image-hero-2.jpg",
         heading: 'We are available all across the globe',
         product: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today." ,
     },
 
     {   
         id: 3,
-        img: "./images/mobile-image-hero-3.jpg",
+        img: "./images/desktop-image-hero-3.jpg",
         heading: 'Manufactured with the best materials',
         product: " Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
     }
@@ -78,8 +78,8 @@ nextBtn.addEventListener('click', () =>{
 //show prev item
 prevBtn.addEventListener('click', () =>{
     currentItem--;
-    if (currentItem > 0) {
-        currentItem = info.lenght - 1;
+    if (currentItem < 0) {
+        currentItem = info.length - 1;
     }
     showNext(currentItem)
 })
